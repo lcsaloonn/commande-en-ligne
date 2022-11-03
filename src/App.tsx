6f2a21e05e066b1257396f74c8aa2fed58dc2c85
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-03 17:18:59 +0100

    setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { MainView } from "views/main.view";
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {/* <Header /> */}
+
+      <div className="app">
+        <Routes>
+          <Route path="/" element={<MainView />} />
+
+          {/* 
+          <Route path="programs">
+            <Route index element={<ProgramListView />} />
+            <Route path=":id" element={<ProgramStartInfoView />} />
+          </Route> */}
+
+          {/* <Route path="*" element={<NotFoundView />} /> */}
+        </Routes>
+      </div>
+    </>
   );
 }
 
