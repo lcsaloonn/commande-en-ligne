b02f9c1ee24f1276d839d07acc1e78533c9c3945
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-04 17:45:30 +0100

    responsive list

--- src/components/base/list-product/listProduct.component.tsx
+++ src/components/base/list-product/listProduct.component.tsx
@@ -1,5 +1,6 @@
 import { ProductCardComponent } from "components/composite/cards";
 import { IProduct } from "types/product/product.interface";
+import "./listProduct.scss";
 
 export function ListProductComponent({
   title,
@@ -13,7 +14,10 @@ export function ListProductComponent({
       <div className="list-product-title">{title}</div>
       <div className="list-product-container grid grid-cols-12 gap-y-6 gap-x-3">
         {productList.map((item: IProduct, id: number) => (
-          <div className="list-product-container-item col-span-6" key={id}>
+          <div
+            className="list-product-container-item col-span-9 xlp:col-span-6"
+            key={id}
+          >
             <ProductCardComponent
               title={item.title}
               text={item.text}
