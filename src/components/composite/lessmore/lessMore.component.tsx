1aef008ab949a5d681071ca0c7b3417fdea6313f
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-04 18:55:32 +0100

    creation lessmore

--- src/components/composite/lessmore/lessMore.component.tsx
+++ src/components/composite/lessmore/lessMore.component.tsx
@@ -1,14 +1,26 @@
 import { useState } from "react";
+import "./lessMore.scss";
 
-export function LessMoreComponent() {
-  const [number, setNumber] = useState(0);
+export function LessMoreComponent({
+  min,
+  max,
+  defaultNumber,
+}: {
+  min: number;
+  max: number;
+  defaultNumber: number;
+}) {
+  const [number, setNumber] = useState(defaultNumber);
   return (
     <div className="less-more">
-      <div className="less-more-remove" onClick={() => setNumber(number - 1)}>
+      <div className="less-more-btn" onClick={() => setNumber(number - 1)}>
         -
       </div>
       <div className="less-more-number">{number}</div>
-      <div className="less-more-add" onClick={() => setNumber(number + 1)}>
+      <div
+        className="less-more-btn items-center"
+        onClick={() => setNumber(number + 1)}
+      >
         +
       </div>
     </div>
