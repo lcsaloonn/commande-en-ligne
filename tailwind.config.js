78cf1e3fa6a17fcfe17b5d4b00c28f0f7e48ef7a
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-04 17:45:07 +0100

    added brakpoint

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,25 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    screens: {
+      xs: "480px",
+
+      sm: "640px",
+
+      md: "768px",
+
+      ml: "950px",
+
+      lg: "1024px",
+
+      lx: "1100px",
+
+      xl: "1280px",
+
+      xlp: "1340px",
+
+      "2xl": "1536px",
+    },
     extend: {},
   },
   corePlugins: {
