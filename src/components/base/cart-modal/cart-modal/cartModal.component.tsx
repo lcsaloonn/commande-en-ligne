b7768e8854dc0feae10e9fbf65fcdd8e42eaced2
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-08 10:00:54 +0100

    remove mock

--- src/components/base/cart-modal/cart-modal/cartModal.component.tsx
+++ src/components/base/cart-modal/cart-modal/cartModal.component.tsx
@@ -1,42 +1,7 @@
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import "./cartModal.scss";
 
-const mock = {
-  title: "Bustina Vert",
-  ingredients:
-    "Mozza fior di latte, Saumon frais, crème de mascarpone citronnée, ciboulette & échalotes coupées finement, épinard frais, parmigiano reggiano 22 mois AOP, tomates cerises.",
-  price: 26.95,
-  additionals: {
-    title: "Pizza",
-    additions: [
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-      {
-        name: "Jambon Parma",
-        price: 2.25,
-      },
-    ],
-  },
-};
+import "./cartModal.scss";
 
 export function CartModalComponent({
   open,
@@ -49,7 +14,6 @@ export function CartModalComponent({
 }) {
   if (open) document.body.classList.add("active-modal");
   else document.body.classList.remove("active-modal");
-
   if (!open) return null;
 
   return (
