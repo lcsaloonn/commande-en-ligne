6efd0faa522b864f696651feb53e3a95f56acc4d
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-08 10:02:21 +0100

    improvement interface product

--- src/types/product/product.interface.ts
+++ src/types/product/product.interface.ts
@@ -1,8 +1,9 @@
 import { IImage } from "types/img.type";
 
 export interface IProduct {
+  id: number;
   title: string;
-  text: string;
+  ingredients: string;
   price: number;
   isDisable: boolean;
   img: IImage;
