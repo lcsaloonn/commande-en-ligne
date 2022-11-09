546b965b3b6432c89b47d72c733fadeaf5044fa1
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-09 17:32:27 +0100

    added responsive on main

--- src/views/main.view.tsx
+++ src/views/main.view.tsx
@@ -15,11 +15,11 @@ export function MainView() {
   return (
     <div className="main main-container">
       <div className="main-view-body grid grid-cols-12 gap-4">
-        <div className="main-view-body-products col-span-8">
+        <div className="main-view-body-products col-span-12 md:col-span-7 xl:col-span-8">
           <ListProductComponent title={"Antipasti"} productList={products} />
         </div>
 
-        <div className="main-view-body-cart col-span-4">
+        <div className="main-view-body-cart md:col-span-5 xl:col-span-4">
           <CartComponent />
 
           <CartModalComponent>
