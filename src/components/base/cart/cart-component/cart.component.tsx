d7000334080b15b527c5ebb496fd35201a3a3af5
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-09 16:42:44 +0100

    added store datas

--- src/components/base/cart/cart-component/cart.component.tsx
+++ src/components/base/cart/cart-component/cart.component.tsx
@@ -1,7 +1,32 @@
+import { useEffect, useState } from "react";
+import { removeFromCart } from "states/features/cart.slice";
+import { useAppDispatch, useAppSelector } from "states/hoocks";
+import { ICartItem } from "types/cart/cart.interface";
 import { CartProductComponent } from "../cart-product/cartProduct.component";
 import "./cart.scss";
 
 export function CartComponent() {
+  const [totalValueCart, setTotalValueCart] = useState(0);
+
+  const cartListProducts = useAppSelector((state) => state.cart);
+  const dispatch = useAppDispatch();
+
+  function removeFormCartList(id: string) {
+    dispatch(removeFromCart(id));
+  }
+
+  function somtotalValueCart() {
+    let total = 0;
+    cartListProducts.items.forEach((items) => {
+      total += items.totalProduct;
+    });
+    setTotalValueCart(total);
+  }
+
+  useEffect(() => {
+    somtotalValueCart();
+  }, [cartListProducts]);
+
   return (
     <div className="cart">
       <div className="cart-content">
@@ -9,13 +34,22 @@ export function CartComponent() {
       </div>
 
       <div className="cart-content-product">
-        <CartProductComponent quantity={1} name={"Burrata"} price={10} />
+        {cartListProducts.items.map((element: ICartItem, key: number) => (
+          <CartProductComponent
+            id={element.id}
+            quantity={element.quantity}
+            name={element.product.title}
+            price={element.totalProduct}
+            onRemoveClick={removeFormCartList}
+            key={key}
+          />
+        ))}
       </div>
 
       <div className="cart-content">
         <div className="cart-content-price">
           <span>Total :</span>
-          <span>37,50 €</span>
+          <span>{totalValueCart.toFixed(2)} €</span>
         </div>
       </div>
 
