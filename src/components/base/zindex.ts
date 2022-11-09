01e71f97fe7a923cccea4036a9774e6a83a5d6e2
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-09 11:01:01 +0100

    refactor names

--- src/components/base/zindex.ts
+++ src/components/base/zindex.ts
@@ -1,13 +1,13 @@
 import { CartModalComponent } from "./cart-modal/cart-modal/cartModal.component";
 import { CartComponent } from "./cart/cart-component/cart.component";
-import { ChooseAdditionalComponent } from "./cart-modal/choose-additionals/chooseAdditionals.component";
+import { ModalContentCartComponent } from "./cart-modal/choose-additionals/modal-content-cart.component";
 import { ListProductComponent } from "./list-product/listProduct.component";
 import { CartProductComponent } from "./cart/cart-product/cartProduct.component";
 
 export {
   ListProductComponent,
   CartModalComponent,
-  ChooseAdditionalComponent,
+  ModalContentCartComponent,
   CartComponent,
   CartProductComponent,
 };
