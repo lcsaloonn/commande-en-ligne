c7bf194a20bb21b332dec8278d6bb7c4f0525f50
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-10 17:13:49 +0100

    cahnge file name

--- src/components/base/zindex.ts
+++ src/components/base/zindex.ts
@@ -1,6 +1,6 @@
 import { CartModalComponent } from "./cart-modal/cart-modal/cartModal.component";
 import { CartComponent } from "./cart/cart-component/cart.component";
-import { ModalContentCartComponent } from "./cart-modal/choose-additionals/modal-content-cart.component";
+import { ModalContentCartComponent } from "./cart-modal/modal-content-cart/modal-content-cart.component";
 import { ListProductComponent } from "./list-product/listProduct.component";
 import { CartProductComponent } from "./cart/cart-product/cartProduct.component";
 
