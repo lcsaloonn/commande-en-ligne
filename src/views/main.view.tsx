38c7132f883c213c5bfce32d9b07c605296fadcb
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-12 16:22:30 +0100

    improvement main

--- src/views/main.view.tsx
+++ src/views/main.view.tsx
@@ -5,35 +5,47 @@ import {
   ModalContentCartComponent,
 } from "components/base/zindex";
 import { useAppSelector } from "states/hoocks";
-import { ProductCategory } from "types/product/product.interface";
+import { IProductCategory } from "types/product-category/product-category.interface";
+import { IProduct } from "types/product/product.interface";
 
 export function MainView() {
-  const products = useAppSelector((state) => state.products).allProducts;
-  const selectedProduct = useAppSelector(
-    (state) => state.products.productSelected
+  const { allProducts: products, productSelected } = useAppSelector(
+    (state) => state.products
   );
+  const productCategory = useAppSelector(
+    (state) => state.category.productCategory
+  );
+
+  function findProductBycateogory(idCategory: number) {
+    let array: IProduct[] = [];
+    products.forEach((element) => {
+      if (element.id === idCategory) {
+        array.push(element);
+      }
+    });
+    return array;
+  }
 
   return (
     <div className="main main-container">
       <div className="main-view-body grid grid-cols-12 gap-4">
         <div className="main-view-body-products col-span-12 md:col-span-7 xl:col-span-8">
-          <ListProductComponent
-            title={"Antipasti"}
-            productList={products}
-            productCategory={ProductCategory.STARTERS}
-          />
-          <ListProductComponent
-            title={"Pizzas"}
-            productList={products}
-            productCategory={ProductCategory.MAIN}
-          />
+          {productCategory.map((element: IProductCategory, key: number) => {
+            return (
+              <ListProductComponent
+                title={element.name}
+                productList={findProductBycateogory(element.id)}
+                key={key}
+              />
+            );
+          })}
         </div>
 
         <div className="main-view-body-cart md:col-span-5 xl:col-span-4">
           <CartComponent />
 
           <CartModalComponent>
-            <ModalContentCartComponent productSelected={selectedProduct} />
+            <ModalContentCartComponent productSelected={productSelected} />
           </CartModalComponent>
         </div>
       </div>
