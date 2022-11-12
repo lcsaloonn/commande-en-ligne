cbd5ab1471284a7e69c6c3f402a5e98e72e9eef2
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-12 16:21:55 +0100

    added category to interface product

--- src/types/product/product.interface.ts
+++ src/types/product/product.interface.ts
@@ -3,18 +3,10 @@ import { IImage } from "types/img.type";
 export interface IProduct {
   id: number;
   title: string;
-  category: ProductCategory;
+  categoryID: number;
   ingredients: string;
   price: number;
   extrasID: number;
   isAvalable: boolean;
   img: IImage;
-}
-
-export enum ProductCategory {
-  STARTERS = 1,
-  MAIN = 2,
-  DERSERT = 3,
-  DRINKS = 4,
-  SIDES = 5,
 }
