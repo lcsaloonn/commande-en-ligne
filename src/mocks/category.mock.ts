783eaf4275b392da87092ffc275a4038f0d353f5
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-12 16:21:05 +0100

    creation category mock

---
+++ src/mocks/category.mock.ts
@@ -0,0 +1,32 @@
+import { IProductCategory } from "types/product-category/product-category.interface";
+
+export const categoryProductMock: IProductCategory[] = [
+  {
+    id: 1,
+    name: "Antipasti",
+  },
+  {
+    id: 2,
+    name: "LaThomàs",
+  },
+  {
+    id: 3,
+    name: "Pizzas",
+  },
+  {
+    id: 4,
+    name: "Plat à emporter",
+  },
+  {
+    id: 5,
+    name: "Desserts",
+  },
+  {
+    id: 6,
+    name: "Softs",
+  },
+  {
+    id: 7,
+    name: "Bières",
+  },
+];
