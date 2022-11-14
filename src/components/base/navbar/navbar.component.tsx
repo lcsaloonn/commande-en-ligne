da5efccfb68adff4c487af8a54426cc4bd980ee5
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-14 11:20:38 +0100

    navbar sticky donewith scroll

--- src/components/base/navbar/navbar.component.tsx
+++ src/components/base/navbar/navbar.component.tsx
@@ -1,26 +1,49 @@
 import { NavBarScrollComponent } from "components/composite/zindex";
+import { useEffect, useState } from "react";
 import { useAppSelector } from "states/hoocks";
 import "./navbar.scss";
 
 export function NavBarComponent() {
+  const [isScroll, setIsScroll] = useState(false);
   const productCategory = useAppSelector(
     (state) => state.category.productCategory
   );
-  return (
-    <div className="nav-bar">
-      <div className="nav-bar-left">
-        <div className="nav-bar-title">
-          <span>La Thomás</span>
-          <span>Ristorante & Pizza</span>{" "}
-        </div>
 
-        <div className="nav-bar-links">
-          <NavBarScrollComponent category={productCategory} />
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 200 && isScroll === false) {
+        setIsScroll(true);
+      } else if (window.scrollY < 200 && isScroll === true) {
+        setIsScroll(false);
+      }
+    };
+    window.addEventListener("scroll", handleScroll, { passive: true });
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [isScroll]);
+
+  if (!isScroll)
+    return (
+      <div className="nav-bar">
+        <div className="nav-bar-left">
+          <div className="nav-bar-title">
+            <span>La Thomás</span>
+            <span>Ristorante & Pizza</span>
+          </div>
+
+          <div className="nav-bar-links">
+            <NavBarScrollComponent category={productCategory} />
+          </div>
+        </div>
+        <div className="nav-bar-right">
+          <img src="../assets/img/header-img.png" alt="" />
         </div>
       </div>
-      <div className="nav-bar-right">
-        <img src="../assets/img/header-img.png" alt="" />
+    );
+  // with styky
+  else
+    return (
+      <div className="nav-bar-sticky">
+        <NavBarScrollComponent category={productCategory} />
       </div>
-    </div>
-  );
+    );
 }
