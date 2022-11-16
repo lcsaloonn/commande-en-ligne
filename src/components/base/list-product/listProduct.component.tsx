4c72717d9d2f7a014938fa5667590337a61d37fd
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-16 12:04:03 +0100

    improvement component return nothing if list is null

--- src/components/base/list-product/listProduct.component.tsx
+++ src/components/base/list-product/listProduct.component.tsx
@@ -10,19 +10,26 @@ export function ListProductComponent({
   title: string;
   productList: IProduct[];
 }) {
-  return (
-    <div className="list-product" id={title}>
-      <div className="list-product-title">{title}</div>
-      <div className="list-product-container grid grid-cols-12 gap-y-6 gap-x-3">
-        {productList.map((item: IProduct, id: number) => (
-          <div
-            className="list-product-container-item col-span-9 xlp:col-span-6"
-            key={id}
-          >
-            <ProductCardComponent product={item} isDisable={!item.isAvalable} />
-          </div>
-        ))}
+  if (productList.length) {
+    return (
+      <div className="list-product" id={title}>
+        <div className="list-product-title">{title}</div>
+        <div className="list-product-container grid grid-cols-12 gap-y-6 gap-x-3">
+          {productList.map((item: IProduct, id: number) => (
+            <div
+              className="list-product-container-item col-span-9 xlp:col-span-6"
+              key={id}
+            >
+              <ProductCardComponent
+                product={item}
+                isDisable={!item.isAvalable}
+              />
+            </div>
+          ))}
+        </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return null;
+  }
 }
