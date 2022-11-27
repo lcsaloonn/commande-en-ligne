eda0e4ef17faee7bd5d32004c5259806edb9876b
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-11-27 14:01:33 +0100

    creation custom select

---
+++ src/components/composite/form/element/select/select.custom.component.tsx
@@ -0,0 +1,41 @@
+import { useEffect, useRef, useState } from "react";
+import "./select.scss";
+
+export default function SelectComponent({
+  data,
+  defaultValue,
+}: {
+  data: any[];
+  defaultValue: string | number;
+}) {
+  const [isActivated, setActivated] = useState(false);
+  const [selectedOption, setSelectedOption] = useState<any>(defaultValue);
+  const textBox = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (textBox.current) textBox.current.value = selectedOption;
+    //returnValue(optionvalue);
+  }, [selectedOption]);
+
+  return (
+    <div
+      className={`dropdown ${isActivated ? "active" : ""}`}
+      onClick={() => setActivated(!isActivated)}
+    >
+      <input type="text" className="textbox" readOnly ref={textBox} />
+      <div className="option">
+        {data.map((data: any, id: number) => (
+          <div
+            onClick={() => {
+              setSelectedOption(data);
+            }}
+            id={data}
+            key={id}
+          >
+            {data}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
