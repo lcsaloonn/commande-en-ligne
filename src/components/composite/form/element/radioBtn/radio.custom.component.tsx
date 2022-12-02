7632380b982e82977c655f1af03c64004ba5673f
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-12-02 14:07:23 +0100

    added default checked

--- src/components/composite/form/element/radioBtn/radio.custom.component.tsx
+++ src/components/composite/form/element/radioBtn/radio.custom.component.tsx
@@ -3,14 +3,16 @@ export default function RadioButtonComponent({
   id,
   family,
   label,
+  isChecked,
 }: {
   id: string;
   family: string;
   label: string;
+  isChecked: boolean;
 }) {
   return (
     <div className="custom-radio">
-      <input type="radio" name={family} id={id} />
+      <input type="radio" name={family} id={id} checked={isChecked} />
       <label htmlFor={id}>{label}</label>
     </div>
   );
