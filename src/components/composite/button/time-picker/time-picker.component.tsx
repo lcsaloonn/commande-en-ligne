668a25a24761d1156380473ed290c73d63450dd7
lcsaloonn <94472108+lcsaloonn@users.noreply.github.com>
2022-12-02 11:19:03 +0100

    creation component  time picker

--- src/components/composite/button/time-picker/time-picker.component.tsx
+++ src/components/composite/button/time-picker/time-picker.component.tsx
@@ -1,32 +1,27 @@
+import { faClock } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { WithdrawalTimeComponent } from "components/base/zindex";
 import ModalComponent from "components/composite/modal/modal.component";
 import { useState } from "react";
 import "./time-picker.scss";
 
-const mock = [
-  ["Sunday", 9.3, 12.0, 15.3, 22.0],
-  ["Monday", 8.3, 12.0, 15.3, 19.0],
-  ["Tuesday", 8.3, 12.0, 15.3, 19.0],
-  ["Wednesday", 8.3, 12.0, 15.3, 19.0],
-  ["Thursday", 8.3, 12.0, 15.3, 19.0],
-  ["Friday", 8.3, 11.3],
-  ["Saturday"],
-];
-
-export default function TimePickerComponent() {
+export default function TimePickerComponent({
+  nextTime,
+}: {
+  nextTime: string;
+}) {
   const [isModalOpen, SetIsModalOpen] = useState(false);
   return (
     <>
-      <button onClick={() => SetIsModalOpen(true)}>thomas</button>
+      <button className="timePicker-btn" onClick={() => SetIsModalOpen(true)}>
+        Retrait de ma commande - {nextTime}
+        <FontAwesomeIcon className="timePicker-icon" icon={faClock} />
+      </button>
       <ModalComponent
         isOpen={isModalOpen}
         onClose={() => SetIsModalOpen(false)}
       >
-        <div>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Animi
-          officiis debitis sequi voluptates ipsam adipisci nisi ullam unde odio
-          quos veniam voluptate, aspernatur, cupiditate perferendis odit! Quia
-          delectus reprehenderit distinctio?
-        </div>
+        <WithdrawalTimeComponent />
       </ModalComponent>
     </>
   );
